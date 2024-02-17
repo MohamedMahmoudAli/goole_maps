be295eee7a0d65f6567512e51473d7e599fb727d
MohamedMahmoudAli <mohamed238ma@gmail.com>
2024-02-17 15:05:59 +0200

    session 10

---
+++ lib/Widgets/custom_google_map.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CustomGoogleMapState extends StatefulWidget {
+  const CustomGoogleMapState({super.key});
+
+  @override
+  State<CustomGoogleMapState> createState() => _CustomGoogleMapStateState();
+}
+
+class _CustomGoogleMapStateState extends State<CustomGoogleMapState> {
+  late  CameraPosition initalcameraPosition;
+  // late  CameraPosition cameraPosition;
+
+   
+
+  @override
+  void initState() {
+      initalcameraPosition=   const CameraPosition(
+        zoom: 10.0,
+         
+        target: LatLng(31.227212467987233, 29.97218040398892)
+        );
+    super.initState();
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    googleMapController.dispose();
+    super.dispose();
+  }
+  late GoogleMapController googleMapController;
+  Widget build(BuildContext context) {
+    return  Stack(
+      children: [
+        GoogleMap(
+          onMapCreated:(controller) {
+            googleMapController=controller;
+          },
+          // cameraTargetBounds: CameraTargetBounds(
+          //   LatLngBounds(northeast:LatLng(31.304149778817113, 30.129218078926055)
+          //  ,southwest:LatLng(31.098571048514753, 29.785458778301486) )
+          //  ),
+          initialCameraPosition:initalcameraPosition 
+          
+            ),
+            Positioned(
+              left: 16,
+              right: 16,
+              bottom: 16,
+              child: ElevatedButton(onPressed: (){
+                
+                googleMapController.animateCamera(CameraUpdate.newLatLng(LatLng(30.317763810885676, 31.191159289815076)));
+                 setState(() {
+                  
+                });
+              }, child: Text("change the postion ")))
+      ],
+    );
+  }
+}
+// world view 0 -> 3
+//country view 4 -> 6
+//city view 10 ->12
+//street view 13->17
+//buildings view 18 ->20
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps/Widgets/custom_google_map.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
@@ -11,25 +12,13 @@ class TestGoogleMapsWithFlutter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:Scaffold(
-      appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("widget.title"),
-      ),
-      body: const Center(
-        child: GoogleMap(
-          initialCameraPosition: CameraPosition(target: LatLng(31,41)
-
-          ,)
-      ),
-      
-    ))
+      home:const CustomGoogleMapState()
 
     );
   }
