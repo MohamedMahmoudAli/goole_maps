9311b9840febb9e7f0b0ed02661d08b16ca19768
MohamedMahmoudAli <mohamed238ma@gmail.com>
2024-02-19 21:25:26 +0200

    save the last edit

--- lib/Widgets/custom_google_map.dart
+++ lib/Widgets/custom_google_map.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_maps/utils/location_services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
@@ -11,51 +11,55 @@ class CustomGoogleMapState extends StatefulWidget {
 }
 
 class _CustomGoogleMapStateState extends State<CustomGoogleMapState> {
-  late  CameraPosition initalCameraPosition;
+  late CameraPosition initalCameraPosition;
   @override
   void initState() {
-      initalCameraPosition=   const CameraPosition(
-        zoom: 0,
-        target: LatLng(0, 0)
-        );
-        location=Location();
-        location.requestPermission();
-
-
+    initalCameraPosition = const CameraPosition(zoom: 0, target: LatLng(0, 0));
+    locationService = LocationService();
+    updateCurrentLocation();
+    googleMapController=GoogleMapController();
     super.initState();
   }
+
   @override
   void dispose() {
     super.dispose();
   }
+
   late Location location;
+  late LocationService locationService;
+  late GoogleMapController googleMapController;
 
   @override
   Widget build(BuildContext context) {
-    return  Stack(
+    return Stack(
       children: [
         GoogleMap(
-          
-          zoomControlsEnabled: false,
-          onMapCreated:(controller) {
-            
-          },
-          initialCameraPosition:initalCameraPosition 
-            ),
-            Positioned(
-              left: 16,
-              right: 16,
-              bottom: 16,
-              child: ElevatedButton(onPressed: (){
-              }, child: const Text("change the postion ")))
+            zoomControlsEnabled: false,
+            onMapCreated: (controller) {},
+            initialCameraPosition: initalCameraPosition),
+        Positioned(
+            left: 16,
+            right: 16,
+            bottom: 16,
+            child: ElevatedButton(
+                onPressed: () {}, child: const Text("change the postion ")))
       ],
     );
   }
-  getLocationPermision()async{
-    var hasPermision =await location.hasPermission();
-    if(hasPermision!=null){
-      location.requestPermission();
 
+  getLocationPermision() async {
+    var hasPermision = await location.hasPermission();
+    if (hasPermision != null) {
+      location.requestPermission();
     }
+  }
+
+  void updateCurrentLocation() async {
+    try {
+      var myLocation = await locationService.getCurrentLocation();
+    } on LocationPermissionException catch (e) {
+    } on LocationServiceException catch (p) {
+    } catch (e) {}
   }
 }
