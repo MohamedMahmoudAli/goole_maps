9333d990922789bcae1269dd0f32a80cabbb844d
MohamedMahmoudAli <mohamed238ma@gmail.com>
2024-02-22 14:33:28 +0200

    add the current location and the marker

--- lib/Widgets/custom_google_map.dart
+++ lib/Widgets/custom_google_map.dart
@@ -12,6 +12,7 @@ class CustomGoogleMapState extends StatefulWidget {
 
 class _CustomGoogleMapStateState extends State<CustomGoogleMapState> {
   late CameraPosition initalCameraPosition;
+  Set<Marker> markers = {};
   @override
   void initState() {
     initalCameraPosition = const CameraPosition(zoom: 0, target: LatLng(0, 0));
@@ -35,24 +36,37 @@ class _CustomGoogleMapStateState extends State<CustomGoogleMapState> {
       children: [
         GoogleMap(
             zoomControlsEnabled: false,
+            mapType: MapType.normal,
+            markers: markers,
             onMapCreated: (controller) {
-              googleMapController;
+              googleMapController = controller;
+              updateCurrentLocation();
             },
             initialCameraPosition: initalCameraPosition),
-        Positioned(
-            left: 16,
-            right: 16,
-            bottom: 16,
-            child: ElevatedButton(
-                onPressed: () {}, child: const Text("change the postion ")))
       ],
     );
   }
+
   void updateCurrentLocation() async {
     try {
       var myLocation = await locationService.getCurrentLocation();
+
+      LatLng currentLocation =
+          LatLng(myLocation.latitude!, myLocation.longitude!);
+      Marker marker = Marker(
+          markerId: MarkerId("CurrentLocation"), position: currentLocation);
+      CameraPosition cameraPosition =
+          CameraPosition(target: currentLocation, zoom: 14);
+      markers.add(marker);
+      googleMapController
+          .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+      setState(() {});
     } on LocationPermissionException catch (e) {
     } on LocationServiceException catch (p) {
     } catch (e) {}
   }
 }
+// create text field 
+// listen to the text field
+// search places
+// display the results
